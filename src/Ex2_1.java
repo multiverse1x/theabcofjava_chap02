ea3eba57fa489bb2ce433719221fa953286b6c60
multiverse1x <multiverse1x@gmail.com>
2022-02-07 21:46:56 +0900

    Ex2_1/Ex2_2 추가

---
+++ src/Ex2_1.java
@@ -0,0 +1,11 @@
+public class Ex2_1 {
+    public static void main(String[] args) {
+
+        //화면에 글자 출력하기
+        //print()와 println()
+
+        System.out.println("Hello, world!!!"); // println은 줄바꿈
+        System.out.println("Hello"); // print()는 줄바꿈x
+        System.out.println("world");
+    }
+}
---
+++ src/Ex2_2.java
@@ -0,0 +1,15 @@
+public class Ex2_2 {
+    public static void main(String[] args) {
+
+        //화면에 글자 출력하기
+        //큰 따옴표 안은 그대로 출력
+        //괄호 안에 큰따옴표 없이 숫자넣고 계산하면 계산됨
+
+        System.out.println("Hello, world");
+        System.out.println("3+5="); // 큰따옴표 안의 내용 그대로 출력
+        System.out.println(3+5); // 사칙연산 식의 결과 계산돼서 출력
+
+
+
+    }
+}
