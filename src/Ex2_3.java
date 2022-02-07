3220ebfa2fd21fc00b85fe7b58b5c4d917a3282f
multiverse1x <multiverse1x@gmail.com>
2022-02-07 21:51:58 +0900

    Ex2_3

---
+++ src/Ex2_3.java
@@ -0,0 +1,16 @@
+public class Ex2_3 {
+    public static void main(String[] args) {
+
+        //계산하기
+        //인텔리제이 너무좋타 >_<
+        //괄호안의 계산식이 계산되어 결과가 출력됨.
+
+
+        System.out.println(5+3);
+        System.out.println(5-3);
+        System.out.println(5*3);
+        System.out.println(5/3); //정수 나누기 정수의 결과는 정수라서 5/3의 결과는 1
+
+
+    }
+}
