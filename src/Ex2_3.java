1431ed34a3930439e1da46ed284574bc406d5e6e
multiverse1x <multiverse1x@gmail.com>
2022-02-07 22:20:36 +0900

    Ex2_3, usageEnvironment

--- src/Ex2_3.java
+++ src/Ex2_3.java
@@ -5,11 +5,22 @@ public class Ex2_3 {
         //인텔리제이 너무좋타 >_<
         //괄호안의 계산식이 계산되어 결과가 출력됨.
 
-
         System.out.println(5+3);
         System.out.println(5-3);
         System.out.println(5*3);
         System.out.println(5/3); //정수 나누기 정수의 결과는 정수라서 5/3의 결과는 1
+        System.out.println();
+        System.out.println("-------------");
+        System.out.println();
+
+
+        // 변수에 저장해서 연산하면 훨씬 더 간편해진다.
+        int x = 5; int y = 3;
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+
 
 
     }
---
+++ src/usageEnvironment.java
@@ -0,0 +1,15 @@
+public class usageEnvironment {
+    public static void main(String[] args) {
+
+        /*사용환경
+
+        HW : 맥 m1에어, 5600x 윈도우10 데탑
+        IDE : 맥 인텔리제이, 윈도우 인텔리제이, 윈도우 이클립스
+        JAVA : java 11 lts
+
+        복습중
+        www.blog.naver.com/multiverse1x
+
+         */
+    }
+}
